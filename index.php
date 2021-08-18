91a9f3d98191da848b7175bce20b59666170cc62
ismailbouras <76954447+ismailbouras@users.noreply.github.com>
2021-08-19 00:54:38 +0100

    F-A

--- index.php
+++ index.php
@@ -19,30 +19,7 @@
 </head>
 
 <body>
-  <header>
-    <div class="logo">
-      <h1 class="logo-text"><span>Awa</span>Inspires</h1>
-    </div>
-    <i class="fa fa-bars menu-toggle"></i>
-    <ul class="nav">
-      <li><a href="#">Home</a></li>
-      <li><a href="#">About</a></li>
-      <li><a href="#">Services</a></li>
-      <!-- <li><a href="#">Sign Up</a></li>
-      <li><a href="#">Login</a></li> -->
-      <li>
-        <a href="#">
-          <i class="fa fa-user"></i>
-          Awa Melvine
-          <i class="fa fa-chevron-down" style="font-size: .8em;"></i>
-        </a>
-        <ul>
-          <li><a href="#">Dashboard</a></li>
-          <li><a href="#" class="logout">Logout</a></li>
-        </ul>
-      </li>
-    </ul>
-  </header>
+  <?php include(?>
 
   <!-- Page Wrapper -->
   <div class="page-wrapper">
